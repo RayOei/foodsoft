5eb1e15a920ff49f6aafaea5a2756ff332f65478
Ray Oei <10501445+RayOei@users.noreply.github.com>
2024-12-30 16:10:00 +0100

    Add check

--- db/migrate/20160217152621_add_pickup_to_order.rb
+++ db/migrate/20160217152621_add_pickup_to_order.rb
@@ -1,5 +1,7 @@
 class AddPickupToOrder < ActiveRecord::Migration[4.2]
   def change
-    add_column :orders, :pickup, :date
+    unless ActiveRecord::Base.connection.column_exists?(:orders, :pickup)    
+      add_column :orders, :pickup, :date
+    end
   end
 end
